b2ec271b2940210024c8a5454baa185088761a1a
BigMoe <jelfcares@gmail.com>
2025-01-04 16:21:55 -0800

    feat: customize wallet modal with bloblife background

--- app/App.jsx
+++ app/App.jsx
@@ -4,7 +4,13 @@ import './App.css';
 
 export default function App() {
   return (
-    <WalletKitProvider>
+    <WalletKitProvider features={['sui:signAndExecuteTransaction']} 
+      customStyle={{
+        '--wkit-modal-background': 'url(/images/background_bloblife.png)',
+        '--wkit-modal-background-size': 'cover',
+        '--wkit-modal-background-position': 'center'
+      }}
+    >
       <div className="App">
         <WalletContent />
       </div>
