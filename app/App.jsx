812de92c66b5535545d57a8f0d61140b4257a423
BigMoe <jelfcares@gmail.com>
2025-01-06 13:23:49 -0800

    add favicon and clean up providers

--- app/App.jsx
+++ app/App.jsx
@@ -1,21 +1,10 @@
-import { WalletKitProvider } from '@mysten/wallet-kit';
-import { WalletContent } from './WalletContent';
-import './App.css';
+import './App.css'
+import WalletContent from './components/WalletContent'
 
 export default function App() {
   return (
-    <WalletKitProvider features={['sui:signAndExecuteTransaction']}
-      customTheme={{
-        '--wkit-accent-hs': '252, 100%',
-        '--wkit-on-accent-rgb': '255, 255, 255',
-        '--wkit-bg-rgb': '0, 0, 0',
-        '--wkit-modal-bg': `url('/images/background_bloblife.png')`,
-        '--wkit-modal-bg-size': 'cover'
-      }}
-    >
-      <div className="App">
-        <WalletContent />
-      </div>
-    </WalletKitProvider>
-  );
+    <div className="App">
+      <WalletContent />
+    </div>
+  )
 }
